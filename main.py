b9dab54bceeddf7c72fd1e366d568ff617f805e7
hewittaj <54426469+hewittaj@users.noreply.github.com>
2023-07-10 18:37:30 +0000

    add art and option to keep playing

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import art
 from helpers import caesar_cipher
 
+print(art.logo)
+keep_playing = True
 direction = input("Type 'e' to encrypt, type 'd' to decrypt:\n")
 
-if direction == 'e' or direction == 'd':
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
-    caesar_cipher(text, shift, direction)
-
-else:
-    print("Invalid option entered. Please try again.")
+while keep_playing:
+    if direction == 'e' or direction == 'd':
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+        caesar_cipher(text, shift, direction)
+        keep_playing = False if input("Would you like to keep playing (y/n)? ") == 'n' else True
+        
+    else:
+        print("Invalid option entered. Please try again.")
